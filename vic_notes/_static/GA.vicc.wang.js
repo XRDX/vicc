e75b71681bb071598145965638609a130bc0cebd
vic <305880887@qq.com>
2018-12-15 19:49:51 +0800

    add static pages

---
+++ vic_notes/_static/GA.vicc.wang.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-120390508-1');
+gtag('config', 'UA-129766101-1');
+
